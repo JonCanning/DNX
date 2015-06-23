f23b8b41805864e03d6309b1dde44080f6fccd79
Jon Canning <jon.canning@gmail.com>
2015-06-23 15:13:48 +0100

    rundnu

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 .dnx\bin\dnvm install latest -Unstable -Persistent
-runDnu
+cmd /c runDnu
