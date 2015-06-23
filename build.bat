4ec3245c001770430a5be2dbb5a3d43f4cd0e2d9
Jon Canning <jon.canning@gmail.com>
2015-06-23 15:26:57 +0100

    meh

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 .dnx\bin\dnvm install latest -Unstable -Persistent
 rem pushd %cd%
-dir %userprofile%\.dnx\runtimes\*
+dir %userprofile%\.dnx\runtimes\*.*
 rem cd %userprofile%\.dnx\runtimes\dnx*\bin
 rem set path=%path%;%cd%
 rem popd
