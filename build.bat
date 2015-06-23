2c0e7a45894fcdab3970f75d736acb29f0eb4d96
Jon Canning <jon.canning@gmail.com>
2015-06-23 15:42:28 +0100

    meh

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 cmd /c %userprofile%\.dnx\bin\dnvm install latest -Unstable -Persistent
+pushd %cd%
+cd %userprofile%\.dnx\runtimes\dnx*\bin
+set path=%path%;%cd%
+popd
 dnu restore
 dnu build --configuration Release
 dnx . test
